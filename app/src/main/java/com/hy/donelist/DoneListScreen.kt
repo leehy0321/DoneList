c869c68279de48b6ea028a6fe64e896ab5ab2311
leehy0321 <sgn0321@naver.com>
2023-11-07 19:42:45 -0800

    - remove useless code

--- app/src/main/java/com/hy/donelist/DoneListScreen.kt
+++ app/src/main/java/com/hy/donelist/DoneListScreen.kt
@@ -2,21 +2,8 @@ package com.hy.donelist
 
 import android.annotation.SuppressLint
 import android.content.Context
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -26,45 +13,15 @@ import com.hy.donelist.ui.ListScreen
 import com.hy.donelist.ui.LoginScreenDisplay
 import com.hy.donelist.ui.NumberScreen
 
-enum class DoneListScreen() {
+enum class DoneListScreen {
     Number,
     List,
-    Create,
     Login
-}
-
-/**
- * Composable that displays the topBar and displays back button if back navigation is possible.
- */
-@Composable
-fun DoneListAppBar(
-    canNavigateBack: Boolean,
-    navigateUp: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    TopAppBar(
-        title = { Text(stringResource(id = R.string.app_name)) },
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
-        ),
-        modifier = modifier,
-        navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.back_button)
-                    )
-                }
-            }
-        }
-    )
 }
 
 @SuppressLint("SuspiciousIndentation")
 @Composable
 fun DoneListApp(
-    modifier: Modifier = Modifier,
     viewModel: DoneListViewModel = viewModel(),
     context: Context
 ) {
@@ -86,7 +43,6 @@ fun DoneListApp(
             composable(route = DoneListScreen.Login.name) {
                 LoginScreenDisplay(
                     onLoginButtonClicked = {
-                        1
                         navController.navigate(DoneListScreen.Number.name)
                     }
                 )
--- app/src/main/java/com/hy/donelist/ui/NumberScreen.kt
+++ app/src/main/java/com/hy/donelist/ui/NumberScreen.kt
@@ -2,19 +2,15 @@ package com.hy.donelist.ui
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,18 +25,13 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hy.donelist.R
-import com.hy.donelist.ui.ui.theme.DoneListTheme
 
 @Composable
 fun NumberScreen(
